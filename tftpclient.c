095b1a57786102883055556ec05f5a7224b894de
Ian Foster <idfoster@ucsd.edu>
2012-10-30 00:25:34 -0700

    replaced the tabs

--- tftpclient.c
+++ tftpclient.c
@@ -10,28 +10,28 @@ int main(int argc, char *argv[])
   struct sockaddr_in      cli_addr, serv_addr;
 
   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-	{
-		printf("%s: can't open datagram socket\n",progname);A
-		exit(1);
+  {
+    printf("%s: can't open datagram socket\n",progname);A
+    exit(1);
   }
 
   //set some struct args
-	bzero((char *) &cli_addr, sizeof(cli_addr));
-	serv_addr.sin_family      = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR);
-	serv_addr.sin_port        = htons(SERV_UDP_PORT);
+  bzero((char *) &cli_addr, sizeof(cli_addr));
+  serv_addr.sin_family      = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR);
+  serv_addr.sin_port        = htons(SERV_UDP_PORT);
 
   //setup the client struct
-	bzero((char *) &cli_addr, sizeof(cli_addr));
-	cli_addr.sin_family      = AF_INET;
-	cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	cli_addr.sin_port        = htons(0);
+  bzero((char *) &cli_addr, sizeof(cli_addr));
+  cli_addr.sin_family      = AF_INET;
+  cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  cli_addr.sin_port        = htons(0);
 
   if (bind(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr)) < 0)
-	{
-		 printf("%s: can't bind local address\n",progname);
-		 exit(1);
-	}
+  {
+     printf("%s: can't bind local address\n",progname);
+     exit(1);
+  }
 
 
   //ready for packet magic here!
